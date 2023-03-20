01756d369d9b97ad07e42adc75f882cdd0ecd660
Estudis <rubenlopezjimenez3@gmail.com>
2023-03-20 15:57:31 +0100

    Canvis tarda de 20/03/2023 v2

---
+++ Catalunya_html.py
@@ -0,0 +1,202 @@
+
+import streamlit.components.v1 as components
+
+
+
+components.html(
+"""<!DOCTYPE html>
+<html>
+<head>
+    <title>ESTUDI D'OFERTA DE NOVA CONSTRUCCIÓ 2022</title>
+</head>
+<body>
+    <h1>ESTUDI D'OFERTA DE NOVA CONSTRUCCIÓ 2022</h1>
+    <p> L’Estudi de l’Oferta d’Habitatge de Nova Construcció a Catalunya que realitza l’Associació de Promotors de
+        Catalunya (APCE) al 2022 ha inclòs un total de 84
+        municipis de Catalunya, en els quals s’han censat 928
+        promocions d’obra nova (87 menys que l’any passat) i un
+        total de 21.796 habitatges, dels quals un 37,5% estan a
+        la venda (8.181, un 30% més que l’any passat).</p>
+    <p>
+        Aquesta oferta activa -en funció de
+        les promocions- és força similar a
+        les províncies de Barcelona i
+        Tarragona (36,2% i 33,2% respecti-
+        vament), i s’incrementa significati-
+        vament a Girona (52,1%) i Lleida
+        (44,8%).
+    </p>
+    <p>
+        De tots els municipis analitzats, el que compta amb més
+        presència d’oferta és el de Barcelona, amb un total de
+        199 promocions i 1.390 habitatges en venda. A
+        continuació els municipis amb més promocions són
+        Sabadell, L’Hospitalet de Llobregat, Badalona, Terrassa i
+        Vilanova i la Geltrú.</p>
+    <p>
+        Dels habitatges en oferta de venda a les promocions analitzades en aquest estudi un 25,5% estan finalitzats i un
+        47,2% es troben en diferents fases constructives. 
+    </p>
+    <p>
+        Per tipologies edificatòries, destaquen els habitatges en plurifamiliar de bloc tancat, concretament el 57,9% dels
+        habitatges, mentre que els de plurifamiliar de bloc obert registren un 38%. A més distància se situen els habitatges
+        unifamiliars adossats (3,8%) i les unifamiliars aïllades (0,4%).
+    </p>
+    <p>
+        La major part dels habitatges inclosos en l’estudi 2022 són d’obra nova (93,6%), reduint-se la rehabilitació integral
+        a un 6,4% (4 dècimes menys que a 2021), majoritàriament concentrada als municipis de la província de Barcelona
+        (88,9%) i de forma destacada al municipi de Barcelona (31,5% del total d’habitatges en venda).
+    </p>
+
+    <h2>Característiques</h2>
+    <p>
+        Les principals tipologies en oferta en els municipis catalans estudiats són els habitatges de 3 dormitoris i 2
+        banys (45,3%). Amb percentatges menors, però significatius es contemplen els habitatges de 2 dormitoris i 2
+        banys (16,8%), els de 2 dormitoris i 1 bany (12,0%), i els de 4 dormitoris i 2 banys (8,8%).
+        El tipus d’habitatge mitjà a la venda a Catalunya per tipologia de dormitoris és el següent: els tipus loft (39); els
+        d’un dormitori (431) els de 2 dormitoris (2.432); els de tres dormitoris (4.138); els de quatre dormitoris (1.088), i
+        els de cinc o més dormitoris (53).
+    </p>
+    <h2>Qualitats i equipaments</h2>
+    <p>
+        Les qualitats més recurrents en els habitatges són:
+        la bomba de calor -fred i calor- (83,6%), la placa
+        d’inducció (69,8%), el parquet (62,5%), els armaris
+        encastats (59,4%), l’aerotèrmia (47,3%), la calefacció
+        (42,9%) la placa de cocció vitroceràmica (21,7%) i les
+        plaques solars (12,5%).
+        Respecte dels equipaments, el més comú és l’ascensor
+        (91,6%), seguit a certa distància per la piscina comu-
+        nitària (50,3%), el traster (48,4%) i la zona enjardinada
+        (37,7%).
+    </p>
+    <h2>Superfície i preus</h2>
+    <p>
+        En general, conforme augmenta la superfície també ho
+        fa el nombre de dormitoris, situant-se la màxima recu-
+        rrència en els habitatges de 3 dormitoris entre els 60 i els
+        90m2, i en els de 2 dormitoris amb superfícies inferiors
+        als 70m2.
+    </p>
+    <p>
+        La mitjana de la superfície útil dels habitatges en venda
+        en els municipis estudiats és de 80,7m2, amb un preu
+        mig de 368.809€ (4.532,6€/m2 útil). Per sota de la mitjana de preu, a nivell general, se situen els habitatges d’un
+        de dos i de tres dormitoris. Si l’anàlisi es fa a partir de la
+        mitjana del preu/m2 útil, per sota de la mitjana resten els
+        habitatges de 3 i 4 dormitoris.
+    </p>
+    <p>
+
+        El 16,8% del conjunt d’habitatges en oferta de venda
+        no supera els 210.000€. Entre aquests habitatges se situen el 46,6% de les d’un dormitori i el 29,2% dels de
+        dos dormitoris. En la banda més alta es troben el 9,9% d’habitatges amb preus superiors als 600.000€, entre els
+        quals es localitza el 66,0% dels de cinc o més dormitoris.
+    </p>
+    <p>
+        Els habitatges unifamiliars obtenen mitjanes de superfície força més altes (155,5m2), així com de preu
+        (515.392€), però el m2 útil (3.302€/m2) se situa per sota
+        la mitjana general, evidenciant que el desplaçament a
+        l’alça del preu no compensa el de la superfície.
+    </p>
+    <p>
+        Els habitatges unifamiliars obtenen mitjanes de superfície força més altes (155,5m2), així com de preu
+        (515.392€), però el m2 útil (3.302€/m2) se situa per sota
+        la mitjana general, evidenciant que el desplaçament a
+        l’alça del preu no compensa el de la superfície.
+    </p>
+    <p>
+        Els habitatges plurifamiliars estan més propers a les
+        mitjanes generals, doncs aporten força més influència sobre aquestes, amb una mitjana de superfície de
+        77,5m2, un preu de venda de 362.492€, i un preu de venda per m2 útil de 4.586€.
+    </p>
+    <h2>Analisi per províncies</h2>
+    <p>
+        Les mitjanes de superfície registrades a les quatre províncies són força similars. Els habitatges dels municipis
+        estudiats de la província de Lleida obtenen la mitjana
+        més alta de superfície (86,5m2), quedant en darrera posició quant a preu (210.299€) i a preu del m2 útil (2.351€).
+        Els municipis de la província de Tarragona obtenen la
+        segona posició en la mitjana de superfície (85,3m2) i se
+        situen en tercera posició quant a preu (244.164€) i quant
+        al preu del m2 de superfície útil (2.958€). Els municipis
+        de la província de Girona obtenen la tercera posició en
+        la mitjana de superfície (82,5m2), i se situen en segona
+        posició respecte al preu mitjà (376.023€) i en el preu m2
+        de superfície útil (4.684€). Finalment, amb la superfície
+        més reduïda (79,6m2) se situen els habitatges dels municipis estudiats a la província de Barcelona, que alhora
+        obtenen el preu més elevat (389.716€) i un preu per m2
+        útil més alt (4.796€).
+    </p>
+    <h2>Vendes i hipoteques</h2>
+    <p>El 66,6% de les vendes les realitza directament el pro-
+        motor, el 6,8% de les vendes es vinculen mitjançant una
+        immobiliària del mateix promotor i el 23,5% es duu a ter-
+        me a través d’una immobiliària independent.</p>
+
+    <h2>Comparativa 2022-2021</h2>
+    <p>
+        El 2022, els municipis estudiats registren un nombre de promocions inferior en 87 unitats respecte de
+        2021, aconseguint les 928. En relació amb el nombre
+        d’habitatges, el total de 2022 (8.181 habitatges) suposa un increment del 30% respecte dels 6.294 habitatges
+        registrats en 2021. Cal fer esment que de les 1.015 promocions de 2021, 601 ja han estat totalment venudes
+        al 2022 i, en relació als habitatges de 2021, el 52,6%
+        han estat venuts. En definitiva, el cens dut a terme el
+        2022 -com va passar l’any anterior- suposa una important renovació de les unitats mostrals: el 55,4% de les
+        promocions són de nova incorporació, i pel que fa als
+        habitatges es van incorporar un 63,5% de nous respecte
+        a l’any 2021.
+    </p>
+    <p>
+        Respecte de les tipologies de promoció, les variacions
+        respecte 2021 són molt atenuades, mantenint-se la seva
+        distribució proporcional. Si aquesta mateixa anàlisi es
+        fa per habitatges, pràcticament no varien els números
+        de 2022 en relació amb 2021 i, lògicament per la seva
+        morfologia, els habitatges plurifamiliars són la majoria
+        (95,9% el 2022 vers 94,7% el 2021).
+    </p>
+    <p>
+        La superfície mitjana dels habitatges a la venda el
+        2022 és de 80,7m2, amb un descens del 4,9% respecte de 2021. Aquest descens es registra en els diferents
+        tipus d’habitatge, a excepció del dels de 5 i més dormi-
+        toris, que registra un increment de superfície del 7,4%.
+        El preu mitjà de l’habitatge a la venda en els municipis
+        estudiats a Catalunya el 2022 és de 368.809€, un 2%
+        menys que el registrat el 2021. Aquest descens de preu
+        es dona només en la tipologia d’habitatges de 3 dormitoris (la més nombrosa), mentre que a la resta de les
+        tipologies el preu s’incrementa de forma molt lleu, entre
+        un 0,8% i un 2%, excepte els habitatges de 5 i més dormitoris, amb un augment del 5,7%, si bé només hi ha 53
+        unitats censades. Pel que fa al preu per m2 útil, en el conjunt dels muni-
+        cipis estudiats, és de 4.533€, valor que suposa un preu
+        superior en un 2,5% respecte del 2021. Si aquestes mateixes consideracions sobre superfície,
+        preu, i preu m2 útil les despleguem sobre l’agrupació
+        d’habitatges unifamiliars i plurifamiliars trobem que:
+    </p>
+    <ul>
+        <div>
+            <li>Els habitatges unifamiliars registren un descens del
+                -9,0% pel que fa a superfície mitjana, i un lleuger -0,9%
+                pel que fa al preu mitjà de venda, mentre que el preu per
+                m2 útil s’incrementa de mitjana un 8,3%.</li>
+        </div>
+        <div>
+            <li>
+                Els habitatges plurifamiliars veuen disminuïda la seva
+                superfície (-3,2%), i presenten variació quant a preu,
+                amb un descens del preu mitjà de venda del -1,6%, i un
+                augment d’un 2,0% pel que fa a preu m2 útil.
+            </li>
+        </div>
+
+    </ul>
+    <p>
+        En aquest sentit, cal recordar que l’evolució de l’IPC en
+        l’exercici 2022 ha estat del 8%.
+    </p>
+</body>
+</html>"""
+
+
+
+)
+
